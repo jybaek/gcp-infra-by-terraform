ff0e300201bf283c425e4dc94089730fc6dec95e
Caley Baek <jybaek@madup.com>
2022-08-13 00:41:21 +0900

    add 20_cloudrun (#3)

---
+++ 20_cloudrun/README.md
@@ -0,0 +1,6 @@
+# intro
+Create an artifact registry and create a cloud run service that takes images from artifact_registry.
+## Architecture
+Below is an architectural image created after Terraform is executed.
+
+![alt text](./images/20_cloudrun.drawio.png)
---
+++ 20_cloudrun/modules/cloud_run_basic/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "project_id"
+  type        = string
+}
+
+variable "service_name" {
+  description = "service_name"
+  type        = string
+}
+
+variable "region" {
+  description = "region"
+  type        = string
+}
+
+variable "repository" {
+  description = "artifact_registry"
+  type        = string
+}
