72d2942fd445e126591fbf6cf291bccd60b36eb9
Caley Baek <jybaek@madup.com>
2022-09-03 18:40:56 +0900

    refactor: remove unused file (#6)

--- 20_cloudrun/modules/artifact_registry/output.tf
+++
@@ -1,4 +0,0 @@
-output "google_artifact_registry_repository" {
-  description = "ID of the created google_artifact_registry_repository id"
-  value       = google_artifact_registry_repository.default.name
-}
