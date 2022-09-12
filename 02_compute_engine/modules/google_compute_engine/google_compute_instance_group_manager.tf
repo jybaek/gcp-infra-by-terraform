3d264b88a07921f93dab50720821b7ac01b60f5a
Caley Baek <jybaek@madup.com>
2022-09-12 21:23:58 +0900

    misc: update fmt

--- 02_compute_engine/modules/google_compute_engine/google_compute_instance_group_manager.tf
+++ 02_compute_engine/modules/google_compute_engine/google_compute_instance_group_manager.tf
@@ -1,15 +1,15 @@
 resource "google_compute_instance_group_manager" "this" {
-  project            = var.project_id
-  name               = "${var.service_name}-vm"
+  project = var.project_id
+  name    = "${var.service_name}-vm"
 
   base_instance_name = "app"
   zone               = var.zone
 
   version {
-    instance_template  = var.instance_template_id
+    instance_template = var.instance_template_id
   }
 
-  target_size  = 2
+  target_size = 2
 
   named_port {
     name = "customhttp"
