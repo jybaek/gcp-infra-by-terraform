4554ab2a8a2215d14185e5f5a2310bd85ccb5643
Caley Baek <jybaek@madup.com>
2022-09-13 23:19:37 +0900

    Modify variable declarations, etc

--- 02_compute_engine/modules/google_cloud_platform/variables.tf
+++ 02_compute_engine/modules/google_cloud_platform/variables.tf
@@ -2,3 +2,13 @@ variable "project_id" {
   description = "project_id"
   type        = string
 }
+
+variable "account_id" {
+  description = "account_id"
+  type        = string
+}
+
+variable "display_name" {
+  description = "display_name"
+  type        = string
+}
