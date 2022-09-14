5cedf923b635b2a9e22987764916f930010056b5
Caley Baek <jybaek@madup.com>
2022-09-14 23:49:48 +0900

    fixed typo

--- 50_cloud_composer/modules/google_compute_engine/output.tf
+++ 50_cloud_composer/modules/google_compute_engine/output.tf
@@ -5,5 +5,5 @@ output "google_compute_network_id" {
 
 output "google_compute_subnetwork_id" {
   description = "ID of the created google_compute_subnetwork id"
-  value       = google_compute_network.this.id
+  value       = google_compute_subnetwork.this.id
 }
